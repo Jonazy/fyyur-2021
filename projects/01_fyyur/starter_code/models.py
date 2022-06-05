c5035c36c7920cf76e6c2c1235afc106bed56252
jonas <jonas.humenu@gmail.com>
2022-06-05 16:10:14 +0100

    fyyur 90% done

---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,62 @@
+from tkinter import CASCADE
+from app import db
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False, nullable=True)
+    seeking_description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f'<Venue {self.name}>'
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate   
+
+class Artist(db.Model):
+    __tablename__ = 'artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    show_id = db.Column(db.Integer, db.ForeignKey('show.id'))
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    genres = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False, nullable=True)
+    seeking_description = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+    def __repr__(self):
+        return f'<Artist {self.name}>'
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+    __tablename__ = 'show'
+    id = db.Column(db.Integer, primary_key=True)
+    image_link = db.Column(db.String(500))
+    artist = db.relationship('Artist', backref='artist', lazy=True)
+    venue = db.relationship('Venue', backref='venue', lazy=True)
+    date = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+         return f'<Show {self.venue} {self.artist}>'
